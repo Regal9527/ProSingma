970e4d652290dcc7135a74b289508db41ee91017
Regal@87 <kalekakr.shekhar@gmail.com>
2023-08-28 15:04:27 +0530

    New substra

---
+++ dif.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+
+public class Sum {
+
+  
+    private static final PrintStream OUT = System.out;
+
+    public static void public static void main(String[] args) {
+        
+        int a = 2 ;
+        int b =3 ;
+        OUT.println(a-b);
+    }
+}
